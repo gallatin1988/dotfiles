763621fd0c51b3c29984acce0f328af63da29295
Sam <svs3@pm.me>
2024-09-18 11:56:22 -0500

    emacs

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -203,12 +203,26 @@
 
 (use-package modus-themes
 :init
-  (setq modus-themes-mode-line '(borderless))
+(setq modus-themes-mode-line '(accented borderless)
+      modus-themes-bold-constructs t
+      modus-themes-italic-constructs t
+      modus-themes-fringes 'subtle
+      modus-themes-tabs-accented t
+      modus-themes-paren-match '(bold intense)
+      modus-themes-completions '((t . (extrabold intense)))
+      modus-themes-org-blocks 'tinted-background
+      modus-themes-scale-headings t
+      modus-themes-region '(bg-only)
+      modus-themes-headings
+      '((1 . (rainbow overline background 1.4))
+        (2 . (rainbow background 1.3))
+        (3 . (rainbow background 1.2))
+        (t . (semilight 1.1))))
 :config
   (load-theme 'modus-vivendi :no-confirm))
-          ;;(use-package catppuccin-theme)
-          ;;(load-theme 'catppuccin t)
-          ;;(catppuccin-reload)
+;;(use-package catppuccin-theme)
+;;(load-theme 'catppuccin :no-confirm)
+;;(catppuccin-reload)
 
 (use-package which-key
 :ensure t
